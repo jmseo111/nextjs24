fe3b153d054878711f61cadf867b51bd8a9eb847
jiminseo <sjm@codeblock.co.kr>
2024-05-05 00:46:31 +0900

    first component

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+export default function Tomato (){
+    return <h1>he next js </h1>
+}
+
+//nextjs 는 프레임워크
+
+//REACT, NEXTJS, REACT-DOM INSTALL
+// NPM INIT -Y
+// LICENSE : MIT
+//APP>PAGE.JSX OR TSX (NAME MUST BE APP. PAGE)
+//REACT IMPORT 해주지 않아도 도미. 넥스트가 알아서 제공해줌.
